1d18a165cbd9c82339baeaddd2cb6175282c6705
Dewaley <adewaleademilua@gmail.com>
2022-02-27 22:34:51 +0100

    Created Tag state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import Input from './components/Input';
 
 function App() {
+  const [tag,setTag] = useState('')
   return (
     <div>
-      <Input />
+      <Input tag = {tag} setTag={setTag}/>
     </div>
   );
 }
