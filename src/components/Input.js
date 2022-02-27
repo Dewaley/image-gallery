8a86537bb1f01c185a262cec82f7fa96df76b8e8
Dewaley <adewaleademilua@gmail.com>
2022-02-27 23:01:52 +0100

    Testing api(stopped cos of network issues)

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const Input = ({ setTag, tag }) => {
-  
+  const submit = async () => {
+    const url = `https://pixabay.com/api/?key=25912123-f6cf2c45649c1add9c51bacc6&q=yellow+flowers&image_type=photo`;
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+  }
   return (
     <div className='flex justify-center items center'>
-      <form className='flex justify-center items-center border-slate-100 border-2  max-w-sm rounded'>
+      <form className='flex justify-center items-center border-slate-100 border-2  max-w-sm rounded' onSubmit={submit}>
         <input
           type='text'
           name=''
@@ -12,7 +17,7 @@ const Input = ({ setTag, tag }) => {
           className='border-none outline-none appearance-none w-full p-2 rounded'
           placeholder='Enter tag'
           value={tag}
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) => setTag(e.target.value)}
         />
         <button type='submit' className='bg-teal-400 text-white p-2 rounded'>
           Search
