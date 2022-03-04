85df4d487364f4f119ce01fa26e4a945b487a3c1
Dewaley <adewaleademilua@gmail.com>
2022-03-04 02:14:10 +0100

    Input data and get Api

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Input from './components/Input';
 
 function App() {
   const [tag,setTag] = useState('')
+  const [result,setResult] = useState([])
+  const submit = async (e) => {
+    e.preventDefault();
+    const url = `https://pixabay.com/api/?key=25912123-f6cf2c45649c1add9c51bacc6&q=${tag}&image_type=photo`;
+    const res = await fetch(url);
+    const data = await res.json();
+    setResult(data)
+  };
   return (
     <div>
-      <Input tag = {tag} setTag={setTag}/>
+      <Input tag = {tag} setTag={setTag} submit={submit}/>
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,22 +1,15 @@
 import React from 'react';
 
-const Input = ({ setTag, tag }) => {
-  const submit = async () => {
-    const url = `https://pixabay.com/api/?key=25912123-f6cf2c45649c1add9c51bacc6&q=yellow+flowers&image_type=photo`;
-    const res = await fetch(url)
-    const data = await res.json()
-    console.log(data)
-  }
+const Input = ({ setTag, tag,submit }) => {
   return (
     <div className='flex justify-center items center'>
-      <form className='flex justify-center items-center border-slate-100 border-2  max-w-sm rounded' onSubmit={submit}>
+      <form className='flex justify-center items-center border-slate-100 border-2  max-w-sm rounded' onSubmit={(e)=>submit(e)}>
         <input
           type='text'
           name=''
           id=''
           className='border-none outline-none appearance-none w-full p-2 rounded'
           placeholder='Enter tag'
-          value={tag}
           onChange={(e) => setTag(e.target.value)}
         />
         <button type='submit' className='bg-teal-400 text-white p-2 rounded'>
