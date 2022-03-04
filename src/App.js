d40ee037bed902f7146021e0cde0f392f0151a66
Dewaley <adewaleademilua@gmail.com>
2022-03-04 22:35:28 +0100

    More functionality

--- src/App.js
+++ src/App.js
@@ -16,15 +16,17 @@ function App() {
     setResults(data.hits);
     const total_count = Math.ceil(data.totalHits / 30);
     setPageCount(total_count);
-    console.log(pageCount)
+    console.log(pageCount);
   };
   const submit = (e) => {
     e.preventDefault();
     getResults();
   };
   const handlePageClick = (data) => {
-    const url = `https://pixabay.com/api/?key=25912123-f6cf2c45649c1add9c51bacc6&q=${tag}&image_type=photo&per_page=30&page=${data.selected+1}`;
-    console.log(url)
+    const url = `https://pixabay.com/api/?key=25912123-f6cf2c45649c1add9c51bacc6&q=${tag}&image_type=photo&per_page=30&page=${
+      data.selected + 1
+    }`;
+    console.log(url);
     fetch(url)
       .then((res) => res.json())
       .then((data) => setResults(data.hits));
@@ -35,7 +37,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         setResults(data.hits);
-        const total_count = Math.ceil(data.totalHits/ 30);
+        const total_count = Math.ceil(data.totalHits / 30);
         setPageCount(total_count);
       });
   }, []);
@@ -44,46 +46,51 @@ function App() {
     <div className='container mx-auto'>
       <Input tag={tag} setTag={setTag} submit={submit} />
 
-      {pageCount > 1 && results !== [] &&(<ReactPaginate
-        previousLabel={'<<'}
-        nextLabel={'>>'}
-        breakLabel={'...'}
-        pageCount={pageCount}
-        onPageChange={handlePageClick}
-        marginPagesDisplayed={2}
-        pageRangeDisplayed={2}
-        containerClassName={'flex justify-center items-center m-3'}
-        pageClassName={
-          'h-6 w-6 border-2 border-r-black border-t-black border-b-black border-l-0 flex justify-center items-center'
-        }
-        pageLinkClassName={'pager-link'}
-        previousClassName={
-          'h-6 w-6 border-2 border-black flex justify-center items-center'
-        }
-        previousLinkClassName={'pager-link'}
-        nextClassName={
-          'h-6 w-6 border-2 border-r-black border-t-black border-b-black border-l-0 flex justify-center items-center'
-        }
-        nextLinkClassName={'pager-link'}
-        breakClassName={
-          'h-6 w-6 border-2 border-r-black border-t-black border-b-black border-l-0 flex justify-center items-center'
-        }
-        breakLinkClassName={'pager-link'}
-      />)}
-      {(<div className='grid sm:grid-cols-3 gap-4 grid-cols-1'>
-        {results.map((result) => (
-          <Result
-            key={result.id}
-            views={result.views}
-            downloads={result.downloads}
-            likes={result.likes}
-            tags={result.tags}
-            user={result.user}
-            image={result.webformatURL}
-            largeImage={result.largeImageURL}
-          />
-        ))}
-      </div>)}
+      {pageCount > 1 && results !== [] && (
+        <ReactPaginate
+          previousLabel={'<<'}
+          nextLabel={'>>'}
+          breakLabel={'...'}
+          pageCount={pageCount}
+          onPageChange={handlePageClick}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={2}
+          containerClassName={'flex justify-center items-center m-3'}
+          pageClassName={
+            'h-6 w-6 border-2 border-r-black border-t-black border-b-black border-l-0 flex justify-center items-center'
+          }
+          pageLinkClassName={'pager-link'}
+          previousClassName={
+            'h-6 w-6 border-2 border-black flex justify-center items-center'
+          }
+          previousLinkClassName={'pager-link'}
+          nextClassName={
+            'h-6 w-6 border-2 border-r-black border-t-black border-b-black border-l-0 flex justify-center items-center'
+          }
+          nextLinkClassName={'pager-link'}
+          breakClassName={
+            'h-6 w-6 border-2 border-r-black border-t-black border-b-black border-l-0 flex justify-center items-center'
+          }
+          breakLinkClassName={'pager-link'}
+        />
+      )}
+        <div className='grid sm:grid-cols-3 gap-4 grid-cols-1'>
+          {results.map((result) => (
+            <Result
+              key={result.id}
+              views={result.views}
+              downloads={result.downloads}
+              likes={result.likes}
+              tags={result.tags}
+              user={result.user}
+              image={result.webformatURL}
+              largeImage={result.largeImageURL}
+            />
+          ))}
+        </div>
+        {results.length === 0 && (
+        <div className='text-xl flex justify-center'>No Items Found</div>
+      )}
     </div>
   );
 }
